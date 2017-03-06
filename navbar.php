15f52d98d40f495cd46010bfebe310fcf398fc2e
hcps-baggsee <hcps-baggsee@180-19BAGGSEE.hcps.internal>
2017-03-06 11:58:56 -0500

    Navbar for PHP Include

---
+++ navbar.php
@@ -0,0 +1,28 @@
+<nav class='navbar navbar-inverse navbar-fixed-top'>
+            <div id='navbar' class='collapse navbar-collapse'>
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                <a class="navbar-brand" href="#">GhostRed Crowd source</a>
+                </div>
+                <ul class='nav navbar-nav'>
+                    <li class='active'>
+                        <a href="#">Home</a>
+                    </li>
+                    
+                    <li>
+                        <a href="#">Problems</a>
+                    </li>
+                    
+                    <li>
+                        <a href="#">Add Problem</a>
+                    </li>
+                    
+                    
+                </ul>
+            </div>
+</nav>
